dde9ea63aa671e7c8a76e2fbe43880571590a5ca
ashackn <ashackn@gmail.com>
2019-01-07 10:03:34 -0700

    updates

---
+++ server.js
@@ -0,0 +1,23 @@
+
+var express = require("express");
+
+var path = require("path");
+var bodyParser = require("body-parser");
+var app = express();
+
+var PORT = 3000;
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" })); 
+
+
+
+// app.use(express.static("public"));
+app.use(require('./app/routing/htmlRoutes'));
+app.use(require('./app/routing/apiRoutes'))
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
