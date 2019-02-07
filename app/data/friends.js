4e9b245f33d1c1c71dbde3cc1acfc72c925c306f
ashackn <ashackn@gmail.com>
2019-02-07 12:59:49 -0700

    changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,53 @@
+var friends = [
+	
+	{
+		name: "Elmo",
+		photo: "https://www.google.com/search?rlz=1C1CHBF_enUS767US767&biw=1920&bih=794&tbm=isch&sa=1&ei=9HZcXPjpCajSjwSYqLvwDw&q=images+of+elmo+valentine&oq=images+of+elmo+valentine&gs_l=img.3...20287.23835..24034...0.0..0.133.666.9j1......1....1..gws-wiz-img.......0j0i30j0i5i30j0i8i30j0i24.bJU5xrgNz6A#imgdii=nouHBatbEeeiMM:&imgrc=GpSYmnhNKm7RLM:",
+		scores: [
+			"3",
+			"5",
+			"4",
+			"3",
+			"1",
+			"5",
+			"3",
+			"1",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "Grinch",
+		photo: "https://www.google.com/search?rlz=1C1CHBF_enUS767US767&biw=1920&bih=794&tbm=isch&sa=1&ei=THdcXL24BpHljwTLtLOQDQ&q=grinch+heart&oq=grinch+heart&gs_l=img.3..0l10.17610.19964..20399...1.0..0.64.402.7......1....1..gws-wiz-img.......0i67.1QSlHE4OBe4#imgrc=8nuluGNxQKl1GM:",
+		scores: [
+			"5",
+			"3",
+			"1",
+			"4",
+			"1",
+			"5",
+			"1",
+			"3",
+			"5",
+			"1"
+		]
+	},
+	{
+		name: "Barney",
+		photo: "https://www.google.com/search?q=barney&rlz=1C1CHBF_enUS767US767&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiVtP-Cn6rgAhUJjoMKHchfARoQ_AUIDygC&biw=1920&bih=794#imgdii=XZUJdFswQTxP7M:&imgrc=SI9tU8bJ_gAcFM:",
+		scores: [
+			"3",
+			"4",
+			"4",
+			"1",
+			"1",
+			"3",
+			"4",
+			"1",
+			"5",
+			"4"		
+		]
+	}
+];
 
-var newFriends = [
-{
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
-    ]
-}
-]
-
-module.exports = newFriends;
+module.exports = friends;
