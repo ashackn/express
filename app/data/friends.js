9302c20db252fd2f316aeef331d6119fcf66fbea
ashackn <ashackn@gmail.com>
2019-02-08 15:11:29 -0700

    changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var friends = [
 	
 	{
 		name: "Elmo",
-		photo: "https://www.google.com/search?rlz=1C1CHBF_enUS767US767&biw=1920&bih=794&tbm=isch&sa=1&ei=9HZcXPjpCajSjwSYqLvwDw&q=images+of+elmo+valentine&oq=images+of+elmo+valentine&gs_l=img.3...20287.23835..24034...0.0..0.133.666.9j1......1....1..gws-wiz-img.......0j0i30j0i5i30j0i8i30j0i24.bJU5xrgNz6A#imgdii=nouHBatbEeeiMM:&imgrc=GpSYmnhNKm7RLM:",
+		photo: "https://i5.walmartimages.com/asr/cca66045-9811-458e-a6d8-030603b0389f_1.e66ee69d9d58134821a2d1622eff5b21.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF",
 		scores: [
 			"3",
 			"5",
@@ -18,7 +18,7 @@ var friends = [
 	},
 	{
 		name: "Grinch",
-		photo: "https://www.google.com/search?rlz=1C1CHBF_enUS767US767&biw=1920&bih=794&tbm=isch&sa=1&ei=THdcXL24BpHljwTLtLOQDQ&q=grinch+heart&oq=grinch+heart&gs_l=img.3..0l10.17610.19964..20399...1.0..0.64.402.7......1....1..gws-wiz-img.......0i67.1QSlHE4OBe4#imgrc=8nuluGNxQKl1GM:",
+		photo: "https://metrouk2.files.wordpress.com/2018/11/sei_38488191-502a.jpg?quality=90&strip=all&zoom=1&resize=644%2C396",
 		scores: [
 			"5",
 			"3",
@@ -34,7 +34,7 @@ var friends = [
 	},
 	{
 		name: "Barney",
-		photo: "https://www.google.com/search?q=barney&rlz=1C1CHBF_enUS767US767&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiVtP-Cn6rgAhUJjoMKHchfARoQ_AUIDygC&biw=1920&bih=794#imgdii=XZUJdFswQTxP7M:&imgrc=SI9tU8bJ_gAcFM:",
+		photo: "https://www.telegraph.co.uk/content/dam/tv/2018/01/24/barney_trans_NvBQzQNjv4BqBmo3ZBPOSwC7GqRZsuXAwDqFnpLh5wjcaIDu-Ojf3tk.jpg?imwidth=450",
 		scores: [
 			"3",
 			"4",
